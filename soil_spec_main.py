693af68ee83e045f313b485f41f30d77801c5e95
yasindu-randil <yr.yasindu@gmail.com>
2022-04-18 11:18:22 -0500

    Add initial hann window transformation
    This commit adds code for hann window transformation.
    Also, it can plot the wavelength for each Clay value.

--- soil_spec_main.py
+++ soil_spec_main.py
@@ -1,54 +1,59 @@
+#%%
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import scipy
+import scipy.signal
 # Read from the CSV file
-df = pd.read_csv('/home/yasindu/Downloads/MIR_spec.csv.part')
-#%%
+df = pd.read_csv('/home/yasindu/Desktop/Soil_Spec/MIR_spec.csv.part')
+
 
 val = 1
 colNames = list( df[df.columns[pd.Series(df.columns).str.startswith('X')]])
 
 
-#%% define a zero series
-padded = np.zeros((1943,1))
-for padCol in range(3600 - 3578):
-   colNames.append("padded" + str(padCol))
-   df["padded" + str(padCol)] = padded
+df2 = df.iloc[1:, 37:]
+dataList = df2.values.tolist()
+
+dataArray = np.array(dataList[0])
+
+ 
+#plt.plot(dataArray)
+hannTransform = scipy.signal.spectrogram(dataArray, window=("hann"))
+
+dataList2 = hannTransform[2]
+#plt.plot(dataList2)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+#plt.show()
+#plt.show(dataList2)
+
+#convert to image
+#minVal = min(dataList2)
+
+for x in range (22):
+    dataArray = np.append(dataArray, [0.0])
+
+minVal = min(dataArray)    
+img = np.reshape(dataArray, (60, 60))
+img = img + abs(minVal)
+maxVal = img.max()
+img = img/ maxVal
+img = img * 255
+plt.imshow(img)
+print(df2.head)
+
 #%%
-subDf = df[colNames]
 
-
-
-# def convertToImage(strCol):
-
-
-#     #print(df.to_string()) 
-
-#     randCol = df.loc[:,strCol]
-
-#     randCol = randCol.to_numpy()
-
-#     colMat = randCol.reshape(29,67)
-
-#     minVal = min(randCol)
-
-
-#     colMat2 = colMat + abs(minVal)
-
-#     maxVal = colMat2.max()
-
-#     colMat2 = colMat2/ maxVal
-
-#     colMat2 = colMat2 * 255
-
-#     plt.imshow(colMat2,  cmap='gray', vmin=0, vmax=255)
-#     # plt.savefig("/home/yasindu/Desktop/Soil_Spec/testImages/soil"+str(val)+".png")
-#     return colMat2
-
-# 
-
+#%% define a zero series
+#padded = np.zeros((1943,1))
+#for padCol in range(3600 - 3578):
+#   colNames.append("padded" + str(padCol))
+#   df["padded" + str(padCol)] = padded
+#%%
+#subDf = df[colNames]
     
 #%%
 def convertToImage(x):
@@ -62,11 +67,7 @@ def convertToImage(x):
     plt.imshow(img,  cmap='gray', vmin=0, vmax=255)
     plt.savefig("/home/yasindu/Desktop/Soil_Spec/testImages/soil"+str(val)+".png")
 
-#%%
-# for x in range(padded.shape[0]):
-#     img = convertToImage(x)
-#     val = val + 1
 
-for x in range (padded.shape[0]):
-    img = convertToImage(x)
-    val = val + 1
+#for x in range (padded.shape[0]):
+#    img = convertToImage(x)
+#    val = val + 1
