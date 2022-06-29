c5f63fd434a498950f264d420c7d2714ac506ee1
yasindu-randil <yr.yasindu@gmail.com>
2022-06-29 12:28:47 -0500

    Update CNN Regression
    Change the model so that it can
    predict a larger range of values
    than previous model.

--- CNN_Regression.py
+++ CNN_Regression.py
@@ -15,7 +15,7 @@ import os.path
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
-
+from tensorflow.keras import models, layers
 from sklearn.metrics import r2_score
 
 import glob
@@ -23,14 +23,15 @@ import glob
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set()  #if you want to use seaborn themes with matplotlib functions
-# # Create DataFrame
+#%% Create DataFrame
 
 
-filepaths = pd.Series(list(glob.iglob('/home/yasindu/Desktop/Datasets/soilimages2/*.png' )), name='Filepath').astype(str)
+filepaths = pd.Series(list(glob.iglob('/home/yasindu/Desktop/Datasets/resize/*.png' )), name='Filepath').astype(str)
 df = pd.read_csv('/home/yasindu/Desktop/Datasets/VNIR_spec.csv', low_memory=False)
+#%%
 df = df.drop(df.iloc[:, 0:34],axis = 1)
 df = df.drop(df.iloc[:, 1:8],axis = 1)
-df = df.drop(df.index[4500:])
+df = df.drop(df.index[10000:])
 dataCols =  df['EOC']
 thresh = 2
 #df2= df[df['EOC']<thresh]
@@ -43,7 +44,7 @@ images= images[images['OC']<thresh]
 
 
 #%%
-print(images)
+#print(images)
 
 
 # # Split the Dataset
@@ -66,10 +67,10 @@ train_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
     x_col='Filepath',
     y_col='OC',
-    target_size=(160, 120),
+    target_size=(32, 32),
     color_mode='rgb',
     class_mode='raw',
-    batch_size=32,
+    batch_size=512,
     shuffle=True,
     seed=42,
     subset='training'
@@ -79,10 +80,10 @@ val_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
     x_col='Filepath',
     y_col='OC',
-    target_size=(160, 120),
+    target_size=(32, 32),
     color_mode='rgb',
     class_mode='raw',
-    batch_size=32,
+    batch_size=512,
     shuffle=True,
     seed=42,
     subset='validation'
@@ -92,17 +93,20 @@ test_images = test_generator.flow_from_dataframe(
     dataframe=test_df,
     x_col='Filepath',
     y_col='OC',
-    target_size=(160, 120),
+    target_size=(32, 32),
     color_mode='rgb',
     class_mode='raw',
-    batch_size=32,
+    batch_size=64,
     shuffle=False
 )
 
-# # Training
-inputs = tf.keras.Input(shape=(160, 120, 3))
+
+#%% Training
+inputs = tf.keras.Input(shape=(32, 32, 3))
 # Extract 16 features
 x = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(inputs)
+
+
 
 x = tf.keras.layers.MaxPool2D()(x)
 #After that inpout was down sampled to 79,59
@@ -113,25 +117,34 @@ x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(x)
 #Downsample again
 x = tf.keras.layers.MaxPool2D()(x) #(38x28)
 
-# Second Convolutional layer to extract 32 features from the downsampled image
-x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(x)
-
-#Downsample again
-x = tf.keras.layers.MaxPool2D()(x) 
 
 #Get the final 64 features
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
+test = tf.keras.layers.GlobalAveragePooling2D()(x)
 
 #@ Hidden layer neural network
-x = tf.keras.layers.Dense(128, activation='relu')(x)
-x = tf.keras.layers.Dense(128, activation='relu')(x)
+x = tf.keras.layers.Dense(64, activation='relu')(test)
+x = tf.keras.layers.Dense(64, activation='relu')(x)
+
 outputs = tf.keras.layers.Dense(1, activation='linear')(x)
 
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
+
+# model = models.Sequential()
+# model.add(layers.Dense(16, input_shape=(126,126,3)))
+# model.add(layers.BatchNormalization())
+# model.add(layers.Dropout(dropout_rate))
+
+# model.add(layers.Dense(32, activation = 'relu'))
+# model.add(layers.BatchNormalization())
+# model.add(layers.Dropout(dropout_rate))
+
+# model.add(layers.Dense(1, activation='relu'))
+
 # The loss function is 'mse', since it is regression
+optimizer = tf.keras.optimizers.Adam(lr=0.0001)
 model.compile(
-    optimizer='adam',
+    optimizer=optimizer,
     loss='mse'
 )
 
@@ -139,7 +152,7 @@ model.compile(
 history = model.fit(
     train_images,
     validation_data=val_images,
-    epochs=100,
+    epochs=10,
     callbacks=[
         tf.keras.callbacks.EarlyStopping(
             monitor='val_loss',
@@ -150,12 +163,16 @@ history = model.fit(
 )
 
 # # Results
-    
+
+#%%    
+#output of the model is the prediction
+# Results
 #output of the model is the prediction
 predicted_OC = np.squeeze(model.predict(test_images))
 
 true_OC = test_images.labels
-
+df10 = pd.DataFrame({'true_OC' : true_OC, 'predicted_OC' : predicted_OC})   
+df10.to_csv('/home/yasindu/Desktop/Datasets/predictions.csv', index=False, encoding='utf-8')
 # SQroot of Mean squared error
 rmse = np.sqrt(model.evaluate(test_images, verbose=0))
 print("     Test RMSE: {:.5f}".format(rmse))
@@ -163,16 +180,33 @@ print("     Test RMSE: {:.5f}".format(rmse))
 
 r2 = r2_score(true_OC, predicted_OC)
 print("Test R^2 Score: {:.5f}".format(r2))
+#plt.figure()
+#sns.scatterplot(x=true_OC, y=predicted_OC, alpha=0.6)
+#sns.lineplot(true_OC, predicted_OC)
+#plt.xlabel('Actual OC', fontsize=14)
+#plt.ylabel('Prediced  OC', fontsize=14)
+#plt.title(f'Actual vs Predicted  OC (test set) T = {thresh}', fontsize=17)
+#plt.show()
 
-sns.scatterplot(x=true_OC, y=predicted_OC, alpha=0.6)
-sns.lineplot(true_OC, predicted_OC)
+print(predicted_OC.max()-predicted_OC.min())
+#sns.regplot(x=true_OC, y=predicted_OC)
 
-plt.xlabel('Actual OC', fontsize=14)
-plt.ylabel('Prediced  OC', fontsize=14)
-plt.title(f'Actual vs Predicted  OC (test set) T = {thresh}', fontsize=17)
+true_OC_max = true_OC.max()
+true_OC_min = true_OC.min()
+
+ax = sns.scatterplot(x=predicted_OC, y=true_OC)
+ax.set(ylim=(true_OC_min, true_OC_max))
+ax.set(xlim=(true_OC_min, true_OC_max))
+ax.set_xlabel("Predicted value of OC")
+ax.set_ylabel("Observed value of OC")
+
+X_ref = Y_ref = np.linspace(true_OC_min, true_OC_max, 100)
+plt.plot(X_ref, Y_ref, color='red', linewidth=1)
 plt.show()
 
-
-
-
+#%%
+his = history.history
+plt.figure()
+plt.plot(his['loss'])
+plt.plot(his['val_loss'])
 
